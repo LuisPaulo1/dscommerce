ffb14736c5ca0653e9fb74c1ea0bd9391bd90ba8
LuisPaulo1 <csluispaulo@gmail.com>
2024-06-11 21:41:41 -0300

    Renderizando catálogo de produtos dinamicamente

--- frontend/src/routes/ClientHome/Catalog/index.tsx
+++ frontend/src/routes/ClientHome/Catalog/index.tsx
@@ -1,22 +1,10 @@
 import SearchBar from '../../../components/SearchBar';
 import CatalogCard from '../../../components/CatalogCard';
 import ButtonNextPage from '../../../components/ButtonNextPage';
+import * as productService from '../../../services/product-service';
 import { ProductDTO } from 'models/product';
 
 import './styles.css';
-
-const product: ProductDTO = {
-  id: 1,
-  name: "Computador Desktop - Intel Core i7",
-  description: "O computador desktop ideal para você que busca desempenho e qualidade.",
-  price: 5000,
-  imgUrl: "https://source.unsplash.com/300x300/?computer",
-  categories: [
-    { id: 1, name: "Computadores" },
-    { id: 2, name: "Eletrônicos" },
-    { id: 3, name: "Importados" }
-  ]
-};
 
 export default function Catalog() {
   return (
@@ -24,18 +12,11 @@ export default function Catalog() {
       <section id="catalog-section" className="dsc-container">
         <SearchBar />
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
+          {
+            productService.findAll().map((product: ProductDTO) => (
+               <CatalogCard key={product.id} product={product} />
+            ))
+          }
         </div>
         <ButtonNextPage />
       </section>
