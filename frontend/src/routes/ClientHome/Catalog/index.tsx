cd3f64138b9baefb3783e508a4b06acbe9168437
LuisPaulo1 <csluispaulo@gmail.com>
2024-06-15 20:06:19 -0300

    Requisições para o catálogo

--- frontend/src/routes/ClientHome/Catalog/index.tsx
+++ frontend/src/routes/ClientHome/Catalog/index.tsx
@@ -1,20 +1,31 @@
 import SearchBar from '../../../components/SearchBar';
 import CatalogCard from '../../../components/CatalogCard';
 import ButtonNextPage from '../../../components/ButtonNextPage';
-import * as productService from '../../../services/product-service';
 import { ProductDTO } from 'models/product';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 import './styles.css';
 
 export default function Catalog() {
+
+  const [products, setProducts] = useState<ProductDTO[]>([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/products?size=12')
+    .then(response => {      
+      setProducts(response.data.content);
+    });
+  }, []);
+
   return (
     <main>
       <section id="catalog-section" className="dsc-container">
         <SearchBar />
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
           {
-            productService.findAll().map((product: ProductDTO) => (
-               <CatalogCard key={product.id} product={product} />
+            products.map((product: ProductDTO) => (
+              <CatalogCard key={product.id} product={product} />
             ))
           }
         </div>
