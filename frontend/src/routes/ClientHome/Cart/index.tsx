1d3ba3d96ae60eb1ccc188b8bdee0f462e62f9c6
LuisPaulo1 <csluispaulo@gmail.com>
2024-06-18 22:18:30 -0300

    Obtendo o carrinho do localStorage

--- frontend/src/routes/ClientHome/Cart/index.tsx
+++ frontend/src/routes/ClientHome/Cart/index.tsx
@@ -1,7 +1,7 @@
-import { useEffect } from 'react';
 import * as cartService from '../../../services/cart.service';
-import './styles.css';
 import { OrderDTO, OrderItemDTO } from '../../../models/order';
+import { useState } from 'react';
+import './styles.css';
 
 const item1 : OrderItemDTO = new OrderItemDTO(4, 1, "PC Gamer", 1200, 
   "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/4-big.jpg")
@@ -11,14 +11,7 @@ const item2 : OrderItemDTO = new OrderItemDTO(5, 2, "Rails for Dummies", 100.99,
 
 export default function Cart() {
 
-  const cart : OrderDTO = new OrderDTO();
-  
-  useEffect(() => {
-    cart.items.push(item1);
-    cart.items.push(item2);
-
-    cartService.saveCart(cart)
-  }, [])
+  const [cart, setCart] = useState<OrderDTO>(cartService.getCart()); 
 
   return (
     <main>
