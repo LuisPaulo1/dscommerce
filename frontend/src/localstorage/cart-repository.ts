8d439649c0e0be5d08ce7a93cbd82178199cba46
LuisPaulo1 <csluispaulo@gmail.com>
2024-06-19 22:44:45 -0300

    Corrigindo o problea do subtotal e total

--- frontend/src/localstorage/cart-repository.ts
+++ frontend/src/localstorage/cart-repository.ts
@@ -1,4 +1,4 @@
-import { OrderDTO } from "../models/order";
+import { OrderDTO, OrderItemDTO } from "../models/order";
 import { CART_KEY } from "../utils/system";
 
 export function save(cart: OrderDTO) {
@@ -8,5 +8,10 @@ export function save(cart: OrderDTO) {
 
 export function get(): OrderDTO {
   const str = localStorage.getItem(CART_KEY) ?? '{"items":[]}'
-  return JSON.parse(str)
+  const obj = JSON.parse(str)
+  const cart = new OrderDTO()
+  obj.items.forEach((p: OrderItemDTO)  => {
+    cart.items.push(new OrderItemDTO(p.productId, p.quantity, p.name, p.price, p.imgUrl))  
+  })
+  return cart
 }
