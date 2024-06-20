5bc5cf8e7aafb0140243883135143fac106eeed8
LuisPaulo1 <csluispaulo@gmail.com>
2024-06-19 21:59:52 -0300

    Adicionando um produto ao carrinho

--- frontend/src/routes/ClientHome/ProductDetails/index.tsx
+++ frontend/src/routes/ClientHome/ProductDetails/index.tsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { ProductDTO } from "models/product";
 import * as productService from '../../../services/product-service';
+import * as cartService from '../../../services/cart.service';
 
 import "./styles.css";
 
@@ -26,6 +27,13 @@ export default function ProductDetails() {
     });
   }, [])
 
+  function handleBuyClick() {
+    if(product){
+      cartService.addProduct(product);
+    }
+    navigate('/cart');
+  }
+
   return (
       <main>
         <section id="product-details-section" className="dsc-container">
@@ -34,7 +42,9 @@ export default function ProductDetails() {
             <ProductDetailsCard product={product} />
           }
           <div className="dsc-btn-page-container">
-            <ButtonPrimary text="Comprar" />
+            <div onClick={handleBuyClick}>
+              <ButtonPrimary text="Comprar" />
+            </div>
             <Link to="/">
               <ButtonInverse text="Início" />            
             </Link>
