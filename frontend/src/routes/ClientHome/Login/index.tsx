1a20429a64f45e2094f7d2e66cad463dacf9fc46
LuisPaulo1 <csluispaulo@gmail.com>
2024-06-28 18:33:46 -0300

    Layout da página Admin

--- frontend/src/routes/ClientHome/Login/index.tsx
+++ frontend/src/routes/ClientHome/Login/index.tsx
@@ -53,7 +53,6 @@ export default function Login() {
                 />
               </div>
             </div>
-
             <div className="dsc-login-form-buttons dsc-mt20">
               <button type="submit" className="dsc-btn dsc-btn-blue">Entrar</button>
             </div>
