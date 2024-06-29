05ded2f9a62ea4f596d8368b46c7be829d084a3b
LuisPaulo1 <csluispaulo@gmail.com>
2024-06-29 18:34:50 -0300

    Decodificando o token para obter o payload

--- frontend/src/services/auth-service.ts
+++ frontend/src/services/auth-service.ts
@@ -1,9 +1,10 @@
 import { AxiosRequestConfig } from "axios";
-import { CredentialsDTO } from "models/auth";
+import { AccessTokenPayloadDTO, CredentialsDTO } from "models/auth";
 import QueryString from "qs";
 import { requestBackend } from "../utils/request";
 import { CLIENT_ID, CLIENT_SECRET } from "../utils/system";
 import * as accessTokenRepository from "../localstorage/access-token-repository";
+import jwtDecode from "jwt-decode";
 
 export function loginRequest(loginData: CredentialsDTO) {
 
@@ -37,4 +38,13 @@ export function saveAccessToken(token: string) {
 
 export function getAccessToken() {
   return accessTokenRepository.get();
+}
+
+export function getAccessTokenPayload(): AccessTokenPayloadDTO | undefined {
+  try {
+    const token = accessTokenRepository.get();
+    return token == null ? undefined : (jwtDecode(token) as AccessTokenPayloadDTO);
+  } catch (error) {
+    return undefined;
+  }
 }
