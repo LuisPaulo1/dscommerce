2824402d7366aaa2b2716231fff60493e948ce44
LuisPaulo1 <csluispaulo@gmail.com>
2024-07-06 23:20:31 -0300

    Função toValues do módulo forms

--- frontend/src/utils/forms.ts
+++ frontend/src/utils/forms.ts
@@ -4,3 +4,11 @@ export function update(inputs: any, name: string, newValue: any) {
     [name]: { ...inputs[name], value: newValue }
   }
 }
+
+export function toValues(inputs: any) {
+  const data: any = {};
+  for (var name in inputs) {
+    data[name] = inputs[name].value;
+  }
+  return data;
+}
