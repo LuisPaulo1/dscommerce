46f4e9f037bbcc0b3710a5aaffe386bc43a9b3aa
LuisPaulo1 <csluispaulo@gmail.com>
2024-07-07 20:18:59 -0300

    Validações e expressões regulares regex

--- frontend/src/routes/Admin/ProductForm/index.tsx
+++ frontend/src/routes/Admin/ProductForm/index.tsx
@@ -17,7 +17,11 @@ export default function ProductForm() {
       id: "name",
       name: "name",
       type: "text",
-      placeholder: "Nome"      
+      placeholder: "Nome",
+      validation: function (value: string) {
+        return /^.{3,80}$/.test(value);
+      },
+      message: "O nome deve ter entre 3 e 80 caracteres"      
     },
     price: {
       value: "",
