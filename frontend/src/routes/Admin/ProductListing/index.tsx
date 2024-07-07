f825442572dd56c840b8d086cd1d7f29e85846bc
LuisPaulo1 <csluispaulo@gmail.com>
2024-07-06 23:39:19 -0300

    Começando a preparar o formulário de produtos

--- frontend/src/routes/Admin/ProductListing/index.tsx
+++ frontend/src/routes/Admin/ProductListing/index.tsx
@@ -7,6 +7,8 @@ import SearchBar from '../../../components/SearchBar';
 import ButtonNextPage from '../../../components/ButtonNextPage';
 import DialogInfo from '../../../components/DialogInfo';
 import DialogConfirmation from '../../../components/DialogConfirmation';
+import ButtonInverse from '../../../components/ButtonInverse';
+import { useNavigate } from 'react-router-dom';
 import './styles.css';
 
 type QueryParams = {
@@ -25,6 +27,8 @@ export default function ProductListing() {
     visible: false,
     message: "Tem certeza?"
   });
+
+  const navigate = useNavigate();
 
   const [isLastPage, setIsLastPage] = useState(false);
 
@@ -71,6 +75,10 @@ export default function ProductListing() {
     setDialogConfirmationData({ ...dialogConfirmationData, visible: false });
   }
 
+  function handleNewProductClick() {
+    navigate('/admin/products/create');    
+  }
+
   function handleDeleteClick(id: number) {
     setDialogConfirmationData({...dialogConfirmationData, id, visible: true});
   }
@@ -80,7 +88,9 @@ export default function ProductListing() {
       <section id="product-listing-section" className="dsc-container">
         <h2 className="dsc-section-title dsc-mb20">Cadastro de produtos</h2>
         <div className="dsc-btn-page-container dsc-mb20">
-          <div className="dsc-btn dsc-btn-white">Novo</div>
+          <div onClick={handleNewProductClick}>
+            <ButtonInverse text="Novo" />
+          </div>
         </div>
         <SearchBar onSearch={handleSearch} />
         <table className="dsc-table dsc-mb20 dsc-mt20">
